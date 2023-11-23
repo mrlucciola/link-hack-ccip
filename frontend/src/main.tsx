5aee8dcd008ce2edb01392d6ff2c750144daaa57
jw418 <ju.wolff@free.fr>
2023-11-23 05:18:17 +0100

    (13) feat: Add .env,dep. and config for the front

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,10 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 // mui
 import CssBaseline from "@mui/material/CssBaseline";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import { lightTheme } from "./mui/theme";
+
 // state
 import { StoreProvider } from "./mobx/provider";
 // components
@@ -12,9 +14,11 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StoreProvider>
-    <ThemeProvider theme={lightTheme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <React.StrictMode>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>
   </StoreProvider>
 );
