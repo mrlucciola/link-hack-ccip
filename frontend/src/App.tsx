91549b1488e61a7a7a9383eb860dfdafe2b89450
jw418 <94624740+jw418@users.noreply.github.com>
2023-11-27 15:51:07 +0100

    (13) feat: Add .env,dep. and config for the front (#27)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,44 @@ import { FC } from "react";
 // mui
 import Grid from "@mui/material/Unstable_Grid2";
 
+import { useState } from "react";
+import chainlinkLogo from "./assets/Chainlink.svg";
+import "./App.css";
+
 const App: FC = () => {
+  const [count, setCount] = useState(0);
+
   return (
-    <Grid
-      className="App"
-      container
-      direction="column"
-      alignItems="center"
-      wrap="nowrap"
-    >
-      App
-    </Grid>
+    <>
+      <div className="title">
+        <a href="https://react.dev" target="_blank">
+          <img
+            src={chainlinkLogo}
+            className="logo chainlink"
+            alt="Chainlink logo"
+          />
+        </a>
+        <span>CCIP Wallet</span>
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+
+      <Grid
+        className="App"
+        container
+        direction="column"
+        alignItems="center"
+        wrap="nowrap"
+      >
+        MUI GRID App
+      </Grid>
+    </>
   );
 };
 
