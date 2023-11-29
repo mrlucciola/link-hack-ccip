177ff64d23e2ca81cf70e3d54a52cdc3b9163b9b
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-11-28 20:23:00 -0500

    (36) refactor: move base components to views and use rootlayout

--- frontend/src/views/Base/Body.tsx
+++ frontend/src/views/Base/Body.tsx
@@ -1,17 +1,16 @@
 import { FC } from "react";
 // mui
-import { Grid } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
 
 const Body: FC = () => {
   return (
     <Grid
-      item
       container
-      flexDirection="column"
+      direction="column"
       flexWrap="wrap"
       flexGrow={1}
-      p="2em"
       overflow="scroll"
+      p="2em"
     >
       body
     </Grid>
