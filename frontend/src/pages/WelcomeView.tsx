c2b38f2bc256926f8215dd3f869023db35188a82
jw418 <94624740+jw418@users.noreply.github.com>
2023-11-29 16:36:25 +0100

    (30) create welcomeview component (#62)
    * (47) feat: add issue-to-pr attr mapping action (#48)
    
    * (30) feat: add WelcomeView component
    
    * (47) feat: add issue-to-pr attr mapping action (#48)
    
    ---------
    
    Co-authored-by: Rocco Lucciola <47836741+mrlucciola@users.noreply.github.com>

---
+++ frontend/src/pages/WelcomeView.tsx
@@ -0,0 +1,40 @@
+import { FC } from "react";
+import { Typography, Button } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
+
+const WelcomeView: FC = () => {
+  return (
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      sx={{
+        textAlign: "center",
+        width: 320,
+        height: 430,
+        margin: "0 auto",
+        position: "relative", // Ajout de position relative ici
+      }}
+    >
+      <Typography
+        variant="h4"
+        component="h1"
+        sx={{
+          position: "absolute",
+          top: "45%",
+          transform: "translateY(-50%)",
+        }}
+      >
+        Welcome to CCIP Wallet
+      </Typography>
+      <Grid sx={{ position: "absolute", bottom: "1.5rem", width: "90%" }}>
+        <Button variant="contained" sx={{ width: "100%" }}>
+          Get started
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default WelcomeView;
