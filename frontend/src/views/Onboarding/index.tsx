61daf2b6890543299a1009fbf6aab1ea7668623b
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-11-28 23:49:43 -0500

    (85) feat: add onboarding root component

---
+++ frontend/src/views/Onboarding/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+// components
+import RootLayout from "../../layouts/RootLayout";
+
+const Onboarding: FC = () => {
+  return <RootLayout></RootLayout>;
+};
+
+export default Onboarding;
