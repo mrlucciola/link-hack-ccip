c3aec6e31c28a553f00f0b10b47abc690ad41a54
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-11-29 01:24:59 -0500

    (31) feat: add state to onboarding

--- frontend/src/views/Onboarding/index.tsx
+++ frontend/src/views/Onboarding/index.tsx
@@ -1,9 +1,37 @@
 import { FC } from "react";
+// state
+import { OnboardingStoreProvider, useOnboardingState } from "./store";
+import { observer } from "mobx-react-lite";
 // components
 import RootLayout from "../../layouts/RootLayout";
 
+const OnboardingView: FC = observer(() => {
+  const currentView = useOnboardingState((s) => s.currentView);
+
+  switch (currentView) {
+    case "keySetup":
+      // return <SetupKeysView />;
+      break;
+    case "splash":
+      break;
+    case "welcome":
+      break;
+    case "complete":
+      break;
+
+    default:
+      break;
+  }
+
+  return <RootLayout>asdf</RootLayout>;
+});
+
 const Onboarding: FC = () => {
-  return <RootLayout></RootLayout>;
+  return (
+    <OnboardingStoreProvider>
+      <OnboardingView />
+    </OnboardingStoreProvider>
+  );
 };
 
 export default Onboarding;
