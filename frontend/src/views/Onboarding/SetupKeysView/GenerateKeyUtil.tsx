27cfdb97559effcd3512c4decca7e4781ed05760
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-11-29 21:34:40 -0500

    (89) feat: add generate new key button

--- frontend/src/views/Onboarding/SetupKeysView/GenerateKeyUtil.tsx
+++ frontend/src/views/Onboarding/SetupKeysView/GenerateKeyUtil.tsx
@@ -1,9 +1,16 @@
 import { FC } from "react";
 // mui
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import { Button } from "@mui/material";
 
 const GenerateKeyUtil: FC = () => {
-  return <Grid>GenerateKeyUtil</Grid>;
+  return (
+    <Grid sx={{ background: "green" }} p={1}>
+      <Button variant="contained" fullWidth>
+        Generate new key
+      </Button>
+    </Grid>
+  );
 };
 
 export default GenerateKeyUtil;
