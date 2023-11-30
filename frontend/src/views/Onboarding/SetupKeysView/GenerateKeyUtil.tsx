226dfec3b3ca10813bb2ea08e46a6ee3b7eed19b
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-11-30 01:04:44 -0500

    (89) feat: add keygen modal with state

--- frontend/src/views/Onboarding/SetupKeysView/GenerateKeyUtil.tsx
+++ frontend/src/views/Onboarding/SetupKeysView/GenerateKeyUtil.tsx
@@ -1,16 +1,149 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+// state
+import { observer } from "mobx-react-lite";
+import { UserWallet, newUserWallet, useOnboardingState } from "../store";
 // mui
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
-import { Button } from "@mui/material";
+import {
+  Box,
+  Button,
+  IconButton,
+  InputAdornment,
+  Modal,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { FaCopy } from "react-icons/fa";
+
+const textInfoStyle = {
+  "& .MuiInputBase-input": {
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+  },
+};
+const CopyIconAdornment = (
+  <InputAdornment position="end">
+    <IconButton edge="end" size="small">
+      <FaCopy />
+    </IconButton>
+  </InputAdornment>
+);
+
+const KeyField: FC<{ label: string; value: string; isMultiline?: boolean }> = ({
+  label,
+  value,
+  isMultiline,
+}) => {
+  return (
+    <TextField
+      fullWidth
+      size="small"
+      disabled
+      variant="filled"
+      sx={textInfoStyle}
+      margin="none"
+      InputProps={{ endAdornment: CopyIconAdornment }}
+      multiline={isMultiline}
+      label={label}
+      value={value}
+    />
+  );
+};
+
+/** ## Open model and generate a new wallet/private key
+ *
+ * Adds randomly generated wallet to state using a setter.
+ */
+const openModal =
+  (handleOpen: () => void, setter: (wallet: UserWallet) => void) => () => {
+    handleOpen();
+    const newWallet = newUserWallet();
+    setter(newWallet);
+  };
 
 const GenerateKeyUtil: FC = () => {
+  const addNewWallet = useOnboardingState((s) => s.addNewWallet);
+  const setCurrentWallet = useOnboardingState((s) => s.setCurrentWallet);
+  const setCurrentWalletAlias = useOnboardingState(
+    (s) => s.setCurrentWalletAlias
+  );
+  const currentWallet = useOnboardingState((s) => s.currentWallet);
+  const [alias, setAlias] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  // event handlers
+  const handleOpen = () => setIsOpen(true);
+  const handleClose = () => {
+    setIsOpen(false);
+    setCurrentWalletAlias(alias);
+    addNewWallet(currentWallet);
+    setAlias("");
+  };
+
   return (
-    <Grid sx={{ background: "green" }} p={1}>
-      <Button variant="contained" fullWidth>
+    <Grid p={1}>
+      <Button
+        variant="contained"
+        fullWidth
+        onClick={openModal(handleOpen, setCurrentWallet)}
+      >
         Generate new key
       </Button>
+      <Modal open={isOpen} onClose={handleClose}>
+        <Box
+          sx={{
+            position: "absolute" as "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: "90%",
+            bgcolor: "background.paper",
+            boxShadow: 24,
+            px: 3,
+            pt: 2,
+            pb: 1,
+            borderRadius: 3,
+            maxHeight: "300px",
+          }}
+          flexWrap="nowrap"
+          overflow="scroll"
+        >
+          <Typography variant="h6" component="h2">
+            Your new key
+          </Typography>
+          <Typography sx={{ my: 1 }}>
+            Please store this information in a secure manner. This message will
+            not display again.
+          </Typography>
+          <Grid container direction="column">
+            <TextField
+              fullWidth
+              size="small"
+              margin="dense"
+              focused
+              label="Set a name for your key:"
+              value={alias}
+              onChange={(e) => setAlias(e.currentTarget.value)}
+            />
+            <KeyField label="Public address" value={currentWallet.address} />
+            <KeyField label="Private key" value={currentWallet.privateKey} />
+            <KeyField
+              label="Secret mnemonic phrase"
+              value={currentWallet.mnemonic?.phrase!}
+              isMultiline={true}
+            />
+          </Grid>
+          <Button
+            fullWidth
+            variant="contained"
+            sx={{ mt: 1 }}
+            onClick={handleClose}
+          >
+            Create key
+          </Button>
+        </Box>
+      </Modal>
     </Grid>
   );
 };
 
-export default GenerateKeyUtil;
+export default observer(GenerateKeyUtil);
--- frontend/src/views/Onboarding/store.tsx
+++ frontend/src/views/Onboarding/store.tsx
@@ -1,4 +1,5 @@
 import { FC, ReactNode } from "react";
+import { HDNodeWallet, Wallet } from "ethers";
 // state
 import { makeAutoObservable } from "mobx";
 import { useLocalObservable } from "mobx-react-lite";
@@ -6,6 +7,16 @@ import { createStoreCtx } from "../../mobx/provider/context";
 import { useStoreData } from "../../mobx/provider/hooks";
 
 export type OnboardingViewType = "keySetup" | "splash" | "welcome" | "complete";
+
+export class UserWallet extends HDNodeWallet {
+  alias: string = "";
+}
+export const newUserWallet = () => {
+  const newWallet = Wallet.createRandom() as UserWallet;
+  newWallet.alias = "";
+
+  return newWallet;
+};
 
 /** Onboarding store
  */
@@ -19,6 +30,8 @@ export class OnboardingStore {
   /////////////////////////////////////////////////////////
   ////////////////////// OBSERVABLES //////////////////////
   currentView: OnboardingViewType = "keySetup";
+  wallets: UserWallet[] = [];
+  currentWallet: UserWallet = {} as UserWallet;
   ////////////////////// OBSERVABLES //////////////////////
   /////////////////////////////////////////////////////////
 
@@ -31,6 +44,15 @@ export class OnboardingStore {
   //////////////////////// ACTIONS ////////////////////////
   setCurrentView(newView: OnboardingViewType) {
     this.currentView = newView;
+  }
+  addNewWallet(newWallet: UserWallet) {
+    this.wallets.push(newWallet);
+  }
+  setCurrentWallet(newWallet: UserWallet) {
+    this.currentWallet = newWallet;
+  }
+  setCurrentWalletAlias(alias: string) {
+    this.currentWallet.alias = alias;
   }
   //////////////////////// ACTIONS ////////////////////////
   /////////////////////////////////////////////////////////
