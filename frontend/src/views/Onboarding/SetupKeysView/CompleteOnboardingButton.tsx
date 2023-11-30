4371012d3081a04ff5313e0d9ebc6bfcb83e8909
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-11-29 21:10:35 -0500

    (31) feat: style completeonboarding button

--- frontend/src/views/Onboarding/SetupKeysView/CompleteOnboardingButton.tsx
+++ frontend/src/views/Onboarding/SetupKeysView/CompleteOnboardingButton.tsx
@@ -2,8 +2,15 @@ import { FC } from "react";
 // mui
 import { Button } from "@mui/material";
 
+/** ## Conclude the onboarding process
+ * Changes the root-curentRootView state to `base`
+ */
 const CompleteOnboardingButton: FC = () => {
-  return <Button>complete onboarding button</Button>;
+  return (
+    <Button variant="contained" sx={{ m: 1 }} fullWidth>
+      Complete setup
+    </Button>
+  );
 };
 
 export default CompleteOnboardingButton;
