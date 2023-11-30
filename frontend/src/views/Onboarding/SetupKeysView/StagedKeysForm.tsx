4b01408f25cf8aa21c3dc91fc405469b725605eb
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-11-30 10:30:38 -0500

    (89) refactor: move staged keys form to its own dir

--- frontend/src/views/Onboarding/SetupKeysView/StagedKeysForm.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-// mui
-import Grid from "@mui/material/Unstable_Grid2/Grid2";
-
-const StagedKeysForm: FC = () => {
-  return <Grid>StagedKeysForm</Grid>;
-};
-
-export default StagedKeysForm;
