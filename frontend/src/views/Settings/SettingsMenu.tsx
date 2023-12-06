f78ab38c17fb0713060891946180bb71193f40b5
jw418 <ju.wolff@free.fr>
2023-12-06 16:38:19 +0100

    (98) wrap List in BodyLayout

--- frontend/src/views/Settings/SettingsMenu.tsx
+++ frontend/src/views/Settings/SettingsMenu.tsx
@@ -1,9 +1,9 @@
 import { FC } from "react";
 import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+import BodyLayout from "../../layouts/BodyLayout";
 
 // MUI
 import { List } from "@mui/material";
-import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import SettingsIcon from "@mui/icons-material/Settings";
 import SettingsMenuOption from "./SettingsMenuOption";
 import SecurityIcon from "@mui/icons-material/Security";
@@ -11,25 +11,17 @@ import DarkModeIcon from "@mui/icons-material/DarkMode";
 
 const SettingsMenu: FC = () => {
   return (
-    <List
-      container
-      direction="column"
-      flexWrap="nowrap"
-      justifyContent="flex-start"
-      overflow="auto"
-      width="100%"
-      maxWidth="100%"
-      px={1}
-      component={Grid}
-    >
-      <SettingsMenuOption icon={<SettingsIcon />} primary={"General"} />
-      <SettingsMenuOption icon={<SecurityIcon />} primary={"Security"} />
-      <SettingsMenuOption
-        icon={<ManageAccountsIcon />}
-        primary={"Manage accounts"}
-      />
-      <SettingsMenuOption icon={<DarkModeIcon />} primary={"Theme"} />
-    </List>
+    <BodyLayout>
+      <List>
+        <SettingsMenuOption icon={<SettingsIcon />} primary={"General"} />
+        <SettingsMenuOption icon={<SecurityIcon />} primary={"Security"} />
+        <SettingsMenuOption
+          icon={<ManageAccountsIcon />}
+          primary={"Manage accounts"}
+        />
+        <SettingsMenuOption icon={<DarkModeIcon />} primary={"Theme"} />
+      </List>
+    </BodyLayout>
   );
 };
 
